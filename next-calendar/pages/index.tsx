5d3406c6403915f2d6eec78f6106c867dd6e79da
n8gallagher <n8gallagher@gmail.com>
2021-08-04 10:55:42 -0700

    Starting calendar app

---
+++ next-calendar/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Welcome to the Next Calendar</h1>
+      <main className={styles.main}>
+      </main>
+
+      <footer className={styles.footer}>
+      </footer>
+    </div>
+  )
+}
