108cf83c2a7ac0543c81d0c075d602bc1eac163c
n8gallagher <n8gallagher@gmail.com>
2021-08-05 10:37:12 -0700

    Creating components for app

---
+++ next-calendar/componenets/calendar/index.tsx
@@ -0,0 +1,14 @@
+import React, { useState }  from "react";
+import dateFns from "date-fns";
+
+function Calendar() {
+    return (
+        <div>
+            <div>Header</div>
+            <div>Days</div>
+            <div>Cells</div>
+        </div>
+    )
+}
+
+export default Calendar;
--- next-calendar/pages/index.tsx
+++ next-calendar/pages/index.tsx
@@ -1,12 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Calendar from '../componenets/calendar/index'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Welcome to the Next Calendar</h1>
       <main className={styles.main}>
+        <Calendar></Calendar>
       </main>
 
       <footer className={styles.footer}>
